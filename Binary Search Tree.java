0f4cd22b420db3ed5ea6eec3837c49aa453b91c4
KIRANKUMARBL <kirandharshan2@gmail.com>
2025-08-12 11:47:13 +0530

    Add files via upload

---
+++ Binary Search Tree.java
@@ -0,0 +1,42 @@
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int value) {
+        data = value;
+        left = right = null;
+    }
+}
+
+class BST {
+    Node root;
+
+    Node insert(Node root, int value) {
+        if (root == null) {
+            return new Node(value);
+        }
+        if (value < root.data) {
+            root.left = insert(root.left, value);
+        } else if (value > root.data) {
+            root.right = insert(root.right, value);
+        }
+        return root;
+    }
+
+    void inorder(Node root) {
+        if (root != null) {
+            inorder(root.left);
+            System.out.print(root.data + " ");
+            inorder(root.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        BST tree = new BST();
+        int[] values = {50, 30, 70, 20, 40, 60, 80};
+        for (int val : values) {
+            tree.root = tree.insert(tree.root, val);
+        }
+        tree.inorder(tree.root);
+    }
+}
